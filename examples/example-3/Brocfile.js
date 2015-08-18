416090c6508c776a21459534adde6c18a3efbe7d
Alan Hogan <contact@alanhogan.com>
2015-08-18 15:34:42 -0700

    Update example 3

--- examples/example-3/Brocfile.js
+++ examples/example-3/Brocfile.js
@@ -3,7 +3,9 @@ var BroccoliEyeglass = require('broccoli-eyeglass');
 var options = {
   cssDir: '/',
 
-  includePaths: ['../../shared-sass-files']
+  includePaths: ['../../shared-sass-files'],
+
+  verbose: true
 };
 
 var outputTree = new BroccoliEyeglass(['src'], options);
